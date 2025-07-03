a561dc06d33dfab3553c8623451c5d0c889cef39
GeraDev <102943430+Gera-Desarrollador-web@users.noreply.github.com>
2025-07-03 04:24:08 -0600

    se agrego ultima muerte

--- src/components/PlayerModal.tsx
+++ src/components/PlayerModal.tsx
@@ -252,6 +252,8 @@ const PlayerModal: React.FC<Props> = ({
         });
     };
 
+    if (!selectedPlayer) return null;
+
     return (
         <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
             <div className="bg-white p-4 rounded shadow-lg w-[90%] max-w-xl max-h-[90vh] overflow-y-auto">
